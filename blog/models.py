3b63f0d0c36a26e470d8666ca5af6544f2001cc2
shreyasthat <shreyasshetty788@gmail.com>
2020-01-25 21:41:09 +0530

    Changed the CSS for the site

--- blog/models.py
+++ blog/models.py
@@ -6,13 +6,14 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+    seller = models.CharField(max_length=200)
     image = models.ImageField()
     description = models.TextField()
     age = models.CharField(max_length=200)
     cost = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
-    #created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
     #published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
