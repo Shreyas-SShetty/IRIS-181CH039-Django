860d1f60f5936a243bd5f22f433701771f7dfc86
Shreyas-SShetty <54276747+Shreyas-SShetty@users.noreply.github.com>
2020-01-31 18:49:52 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,12 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 
+#Post is  the name of the model
+#models.Model means that the post is a Django model
+
 class Post(models.Model) :
-    author = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
+    #properties of Django model
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
     name = models.CharField(max_length=200)
     seller = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/', null=True, blank=True)
@@ -28,12 +32,3 @@ class Post(models.Model) :
 
     def __str__(self) :
         return self.author
-
-
-class UserProfile(models.Model) :
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
-    wallet = models.IntegerField(default=5000)
-
-    def __str__(self) :
-        return self.user.username
-
